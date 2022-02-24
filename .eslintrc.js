4b43170cab70c820d8c4da83d9f7937ffeee8df3
slozanoa98 <santiagolozal@outlook.com>
2022-02-24 07:47:00 -0500

    confuguracion

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
 module.exports = {
-  root:true,
-  env:{
-    browser:true,
+  root: true,
+  env: {
+    browser: true,
     amd: true,
-    node:true,
-    es6:true
+    node: true,
+    es6: true,
   },
-  extends:[
-    "eslint:recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended",
-    "next",
-    "next/core-web-vitals"
-  ],
+  extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
   rules: {
-    "semi": ['error', 'always']
-  }
-}
+    semi: ['error', 'always'],
+  },
+};
