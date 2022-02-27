e0c38c0398501449b0712e3d934ecd93e5e1ea82
slozanoa98 <santiagolozal@outlook.com>
2022-02-26 22:19:45 -0500

    configuración general para proyecto nextjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
     amd: true,
     node: true,
     es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
+  extends: [
+    "eslint:recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "next",
+    "next/core-web-vitals",
+  ],
   rules: {
-    semi: ['error', 'always'],
+    semi: ["error", "always"],
   },
 };
