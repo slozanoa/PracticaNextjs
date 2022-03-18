1edb2c6c430499e2f589bdae2d8c35c022052ba7
slozanoa98 <santiagolozal@outlook.com>
2022-03-18 16:33:15 -0500

    deploy vercel 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,5 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
   rules: {
     semi: ['error', 'always'],
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
   },
 };
